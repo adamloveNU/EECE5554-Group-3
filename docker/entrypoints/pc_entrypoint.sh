fdaaad1a9d6734fc15bed2b78b552a91bf7c2ad6
Adam Love <love.a@northeastern.edu>
2025-11-20 17:58:39 -0500

    create more dockerfiles

--- docker/entrypoints/pc_entrypoint.sh
+++ docker/entrypoints/pc_entrypoint.sh
@@ -3,9 +3,10 @@
 set -e
 
 # source all needed setup scripts
-source /opt/ros/jazzy/setup.sh
-source /EECE5554/cv_workspace/install/setup.sh
-source /EECE5554/rpi_workspace/install/setup.sh
+. /opt/ros/jazzy/setup.sh
+. /EECE5554/cv_workspace/install/setup.sh
+. /EECE5554/rpi_workspace/install/setup.sh
+. /EECE5554/pc_workspace/install/setup.sh
 
 # run the command passed through on the command line (if included)
 exec "$@"
